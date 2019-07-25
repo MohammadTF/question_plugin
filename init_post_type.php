4025018b9166c66ef14453b6cda478f35a97c87a
Tasneem Faiziyab <tasneem@kiffgo.io>
2019-07-25 20:17:25 +0500

    first commit

---
+++ init_post_type.php
@@ -0,0 +1,138 @@
+<?php
+
+class Gohar_e_Hikmat_Questions{
+    private static $instance = null;
+    public function __construct()    
+    {
+        add_action( 'init', array($this,'codex_question_init') );
+        add_action( 'admin_menu', array($this,'rudr_add_a_metabox') );
+        add_action( 'save_post', array($this,'rudr_save_post_meta'), 10, 2 );
+
+    }
+
+    /**
+     * Register a question post type.
+     *
+     * @link http://codex.wordpress.org/Function_Reference/register_post_type
+     */
+    function codex_question_init() {
+        $labels = array(
+            'name'               => _x( 'Questions', 'post type general name', 'your-plugin-textdomain' ),
+            'singular_name'      => _x( 'Question', 'post type singular name', 'your-plugin-textdomain' ),
+            'menu_name'          => _x( 'Questions', 'admin menu', 'your-plugin-textdomain' ),
+            'name_admin_bar'     => _x( 'Question', 'add new on admin bar', 'your-plugin-textdomain' ),
+            'add_new'            => _x( 'Add New', 'question', 'your-plugin-textdomain' ),
+            'add_new_item'       => __( 'Add New question', 'your-plugin-textdomain' ),
+            'new_item'           => __( 'New question', 'your-plugin-textdomain' ),
+            'edit_item'          => __( 'Edit question', 'your-plugin-textdomain' ),
+            'view_item'          => __( 'View question', 'your-plugin-textdomain' ),
+            'all_items'          => __( 'All questions', 'your-plugin-textdomain' ),
+            'search_items'       => __( 'Search questions', 'your-plugin-textdomain' ),
+            'parent_item_colon'  => __( 'Parent questions:', 'your-plugin-textdomain' ),
+            'not_found'          => __( 'No questions found.', 'your-plugin-textdomain' ),
+            'not_found_in_trash' => __( 'No questions found in Trash.', 'your-plugin-textdomain' )
+        );
+
+        $args = array(
+            'labels'             => $labels,
+            'description'        => __( 'Description.', 'your-plugin-textdomain' ),
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'question' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+        );
+
+        register_post_type( 'question', $args );
+    }
+
+    function rudr_add_a_metabox() {
+        add_meta_box(
+            'gohar_e_hikmat_question_box', // metabox ID, it also will be the HTML id attribute
+            'Questions', // title
+            array($this,'question_display_metabox'), // this is a callback function, which will print HTML of our metabox
+            'question', // post type or post types in array
+            'normal', // position on the screen where metabox should be displayed (normal, side, advanced)
+            'default' // priority over another metaboxes on this page (default, low, high, core)
+        );
+    }
+    function rudr_save_post_meta( $post_id, $post ) {
+        
+        /* 
+         * Security checks
+         */
+        if ( !isset( $_POST['rudr_metabox_nonce'] ) 
+        || !wp_verify_nonce( $_POST['rudr_metabox_nonce'], 'gh_question_save' ) )
+            return $post_id;
+        /* 
+         * Check current user permissions
+         */
+        $post_type = get_post_type_object( $post->post_type );
+        if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+            return $post_id;
+        /*
+         * Do not save the data if autosave
+         */
+        if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+            return $post_id;
+        
+            
+            if ($post->post_type == 'question') { // define your own post type here
+                print_r($_POST['gh_question']);
+                print_r($_FILES);
+                die('sdf');
+            update_post_meta($post_id, 'gohar_e_hikmat_questions',  $_POST['gh_question']  );
+            print_r([$_POST['gh_question'],$_FILES['gh_pdf']]);
+            if( ! empty( $_FILES ) && isset( $_FILES['gh_pdf'] ) ) {
+            // Upload the goal image to the uploads directory, resize the image, then upload the resized version
+				$goal_image_file = wp_upload_bits( $_FILES['gh_pdf']['name'], null, wp_remote_get( $_FILES['gh_pdf']['tmp_name'] ) );
+
+				if( false == $goal_image_file['error'] ) {
+				
+					// Since we've already added the key for this, we'll just update it with the file.
+					update_post_meta( $post_id, 'gohar_e_hikmat_pdf', $goal_image_file['url'] );
+		
+				} // end if/else
+			} // end if/else
+				//
+        }
+       
+        return $post_id;
+    }
+    
+    function question_display_metabox( $post ) {
+       
+
+        $template_name = 'question_metabox';
+        ob_start();
+    
+        do_action( 'personalize_login_before_' . $template_name );
+    
+        require( 'templates/' . $template_name . '.php');
+    
+        do_action( 'personalize_login_after_' . $template_name );
+    
+        $html = ob_get_contents();
+        ob_end_clean();
+    
+        echo $html;
+        
+    }
+    
+
+    public static function getInstance()
+    {
+      if(!self::$instance)
+      {
+        self::$instance = new Gohar_e_Hikmat_Questions();
+      }
+     
+      return self::$instance;
+    }
+}
---
+++ templates/question_metabox.php
@@ -0,0 +1,166 @@
+<div hidden>
+<p><label>SEO title <input type="text" name="gh_question_title"
+ value="<?php echo esc_attr( get_post_meta($post->ID, 'gh_question_title',true) );?>" /></label></p>
+<p><label><input type="checkbox" name="gh_question_noindex" 
+<?php echo checked( get_post_meta($post->ID, 'gh_question_noindex',true), 'on', false ); ?>
+         /> Turn off page visibility for search engines</label></p>
+</div>
+
+<?php
+/*
+	 * needed for security reasons
+	 */
+    wp_nonce_field( 'gh_question_save', 'rudr_metabox_nonce' );
+    $data = get_post_meta($post->ID, 'gohar_e_hikmat_questions',true);
+    $link = get_post_meta($post->ID, 'gohar_e_hikmat_pdf',true);
+?>
+<div>
+    <label for="gh_pdf">Upload PDF:</label>
+    <input type="file" name="gh_pdf" id="gh_pdf">
+    <?php if($link): ?>
+    <a href="<?php echo $link;?>" target="_blank">View</a>
+    <?php endif; ?>
+
+</div>
+
+<div id="gh_question_wrapper">
+<?php if(empty($data)): ?>
+    <div class="gh_question_inner">
+        <div>
+            <label for="question_1">Question:</label>
+            <input type="text" name="gh_question[0][title]" value="" id="question_1">
+            
+        </div>
+       
+        <div>
+            <label for="correct_answer_1">Correct Answer:</label>
+            <input type="text" name="gh_question[0][correct_answer]" value="" id="correct_answer_1">
+            
+        </div>
+        <div>
+            <div>
+                <label for="option_1_1">Option 1</label>
+                <input type="text" class="options" id="option_1_1" name="gh_question[0][option][]" value="" >
+                
+            </div>
+            <div>
+                <label for="option_1_2">Option 2</label>
+                <input type="text" class="options" id="option_1_2" name="gh_question[0][option][]" value="" >
+            </div>
+            <div>
+                <label for="option_1_3">Option 3</label>
+                <input type="text" class="options" id="option_1_3" name="gh_question[0][option][]" value="" >
+            </div>
+            <!-- <div>
+                <label for="option_1_4">Option 4</label>
+                <input type="text" class="options" id="option_1_4" name="gh_question[0][option][]" value="" >
+            </div> -->
+        </div>
+        <div>
+            <a href="#" class="gh_clone-btn gh_add-btn">Add Question</a>
+            <a href="#" class="gh_clone-btn gh_rem-btn">Remove Question</a>            
+        </div>
+
+    </div>
+
+<?php else: ?>
+<?php
+foreach($data as $d=>$v)
+{
+    $index = $d + 1;
+?>
+ 
+
+<div class="gh_question_inner">
+        <div>
+            <label for="question_<?php echo $index;?>">Question:</label>
+            <input type="text" name="gh_question[<?php echo $d;?>][title]" value="<?php echo $v['title']?>" id="question_<?php echo $index;?>">
+            
+        </div>
+       
+        <div>
+            <label for="correct_answer_<?php echo $index;?>">Correct Answer:</label>
+            <input type="text" name="gh_question[<?php echo $d;?>][correct_answer]" value="<?php echo $v['correct_answer']?>" id="correct_answer_<?php echo $index;?>">
+            
+        </div>
+        <div>
+            <div>
+                <label for="option_<?php echo $index;?>_1">Option 1</label>
+                <input type="text" class="options" id="option_<?php echo $index;?>_1" name="gh_question[<?php echo $d;?>][option][0]" value="<?php echo $v['option'][0]?>" >
+                
+            </div>
+            <div>
+                <label for="option_<?php echo $index;?>_2">Option 2</label>
+                <input type="text" class="options" id="option_<?php echo $index;?>_2" name="gh_question[<?php echo $d;?>][option][1]" value="<?php echo $v['option'][1];?>" >
+            </div>
+            <div>
+                <label for="option_<?php echo $index;?>_3">Option 3</label>
+                <input type="text" class="options" id="option_<?php echo $index;?>_3" name="gh_question[<?php echo $d;?>][option][2]" value="<?php echo $v['option'][2]?>" >
+            </div>
+            <!-- <div>
+                <label for="option_1_4">Option 4</label>
+                <input type="text" class="options" id="option_1_4" name="gh_question[0][option][]" value="" >
+            </div> -->
+        </div>
+        <div>
+            <a href="#" class="gh_clone-btn gh_add-btn">Add Question</a>
+            <a href="#" class="gh_clone-btn gh_rem-btn">Remove Question</a>            
+        </div>
+
+    </div>
+<?php
+}
+ ?>
+<?php endif; ?>
+</div>
+<script>
+jQuery().ready(function($){
+    $('.gh_add-btn').on('click',function(e){
+        e.preventDefault();
+        var length  = $('.gh_question_inner').length;
+        var index   = +length + 1;
+        
+        var clonned = $('.gh_question_inner').first().clone(true);
+        clonned.find('label[for="question_1"]').attr("for","question_"+index);
+        clonned.find('label[for="question_'+index+'"]').next().attr("id","question_"+index)
+                                            .attr("name","gh_question["+index+"][title]")
+                                            .attr("val","");
+
+        clonned.find('label[for="correct_answer_1"]').attr("for","correct_answer_"+index);
+        clonned.find('label[for="correct_answer_'+index+'"]').next().attr("id","correct_answer_"+index)
+                                            .attr("name","gh_question["+index+"][correct_answer]")
+                                            .attr("val","");
+        
+        clonned.find('label[for="option_1_1"]').attr("for","option_"+index+"_1");
+        clonned.find('label[for="option_'+index+'_1"]').next().attr("id","option_"+index+"_1")
+                                            .attr("name","gh_question["+index+"][option][]")
+                                            .attr("val","");
+
+        clonned.find('label[for="option_1_2"]').attr("for","option_"+index+"_2");
+        clonned.find('label[for="option_'+index+'_2"]').next().attr("id","option_"+index+"_2")
+                                            .attr("name","gh_question["+index+"][option][]")
+                                            .attr("val","");
+
+        clonned.find('label[for="option_1_3"]').attr("for","option_"+index+"_3");
+        clonned.find('label[for="option_'+index+'_3"]').next().attr("id","option_"+index+"_3")
+                                            .attr("name","gh_question["+index+"][option][]")
+                                            .attr("val","");
+
+        // clonned.find('label[for="option_1_4"]').attr("for","option_"+index+"_4");
+        // clonned.find('label[for="option_'+index+'_4"]').next().attr("id","option_"+index+"_4")
+        //                                     .attr("name","gh_question["+index+"][option][]")
+        //                                     .attr("val","");
+        debugger;
+        $('#gh_question_wrapper').append(clonned);
+
+    });
+    $('.gh_rem-btn').on('click',function(e){
+        e.preventDefault();
+        if($('.gh_question_inner').length > 1)
+        {
+            $(this).parent().parent().remove();
+        }
+        
+    });
+});
+</script>
